b4a603228bf840f61fd066854ff32ae0fb90466b
PS <89146019257@mail.ru>
2022-05-29 22:58:09 +0900

    Initial commit

---
+++ src/test/java/ru/netology/data/CardDeliveryTest.java
@@ -0,0 +1,43 @@
+package ru.netology.data;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import ru.netology.delivery.data.DataGenerator;
+
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static ru.netology.delivery.data.DataGenerator.generateDate;
+
+public class CardDeliveryTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @BeforeEach
+    public void setUp() {
+        open("http://localhost:9999/");
+    }
+
+    @Test
+    void shouldGenerateTestAndRescheduleTheMeeting(){
+
+    }
+}
