94da885c60590c00d9e6e1aacca94a6f208d9b00
PS <89146019257@mail.ru>
2022-05-29 23:17:19 +0900

    add test

--- src/test/java/ru/netology/data/CardDeliveryTest.java
+++ src/test/java/ru/netology/data/CardDeliveryTest.java
@@ -38,6 +38,30 @@ public class CardDeliveryTest {
 
     @Test
     void shouldGenerateTestAndRescheduleTheMeeting(){
-
+        String planningDate = generateDate(4);
+        Configuration.holdBrowserOpen=true;
+        open("http://localhost:9999");
+        $("[placeholder=\"Город\"]").setValue(DataGenerator.generateCity());
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[placeholder=\"Дата встречи\"]").setValue(planningDate);
+        $("[name=\"name\"]").setValue(DataGenerator.generateName());
+        $("[name=\"phone\"]").setValue("+").setValue(DataGenerator.generatePhone());
+        $("[data-test-id=\"agreement\"]").click();
+        $x("//*[text()=\"Запланировать\"]").click();
+        $(withText("Успешно!")).should(visible, Duration.ofSeconds(15));
+        $("[data-test-id='success-notification'] > .notification__content").shouldHave(exactText("Встреча успешно " +
+                "запланирована на " + planningDate));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[placeholder=\"Дата встречи\"]").setValue(DataGenerator.forwardDate(8));
+        $(withText("Запланировать")).click();
+        $("[data-test-id='replan-notification'] .notification__title").shouldHave(exactText("Необходимо подтверждение"))
+                .shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id='replan-notification'] .notification__content").shouldHave(text("У вас уже" +
+                " запланирована встреча на другую дату. Перепланировать?"));
+        $(withText("Перепланировать")).click();
+        $("[data-test-id='success-notification'] .notification__title").shouldHave(exactText("Успешно!"))
+                .shouldBe(Condition.visible);
+        $("[data-test-id='success-notification'] .notification__content").shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно запланирована на " + DataGenerator.forwardDate(8)));
     }
 }
